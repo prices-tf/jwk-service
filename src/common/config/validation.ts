b9607f394215231384432d531a3a11b76edb2ffe
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-10-13 22:44:23 +0200

    feat: add default data directory

--- src/common/config/validation.ts
+++ src/common/config/validation.ts
@@ -3,7 +3,7 @@ import * as Joi from 'joi';
 const validation = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production', 'test').required(),
   PORT: Joi.number().default(3000),
-  DATA_DIR: Joi.string().required(),
+  DATA_DIR: Joi.string().default('/data'),
 });
 
 export { validation };
